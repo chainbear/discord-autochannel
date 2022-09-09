7df4c69786e00718d48991cbeb8f1b1ae4aac1a9
Linus <git@linus9000.de>
2022-09-09 12:39:02 +0200

    allow reading channel names from a config file (#4)

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_MEMBERS] });
+'use strict';
+
+const fs = require('fs');
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMembers] });
 
 const magic_channel_name = "Join Me!";
 const channel_prefix = "[AC]";
-const channel_names = [
+let channel_names = [
     'Eisen 2',
     'ezpz',
     'Holzauge',
@@ -16,21 +20,29 @@ const channel_names = [
     'Sweating',
 ];
 
+try {
+    channel_names = fs.readFileSync('/etc/discord-autochannel/channel_names').toString().split("\n");
+    console.log(`Got ${channel_names.length} channel names from config file`);
+}
+catch (e) {
+    console.warn("Could not read channel names from file, using default names.", e);
+}
+
 let managed_channels = [];
 client.on('ready', async (client) => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.channels.cache.forEach(channel => {
-    if (channel.name.startsWith(channel_prefix)) {
-        if (channel.members.size > 0) {
-            managed_channels.push(channel);
-        } else {
-            channel.delete();
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.channels.cache.forEach(channel => {
+        if (channel.name.startsWith(channel_prefix)) {
+            if (channel.members.size > 0) {
+                managed_channels.push(channel);
+            } else {
+                channel.delete();
+            }
         }
-    }
-    if (channel.name === magic_channel_name) {
-        channel.members.forEach(member => create_channel_for_member(member, channel));
-    }
-  });
+        if (channel.name === magic_channel_name) {
+            channel.members.forEach(member => create_channel_for_member(member, channel));
+        }
+    });
 
 });
 
@@ -38,21 +50,24 @@ client.on('voiceStateUpdate', async (before, after) => {
     if (before.channel != null && managed_channels.includes(before.channel)) {
         if (before.channel.members.size == 0) {
             managed_channels = managed_channels.filter(channel => channel !== before.channel);
+            console.log(`Deleting channel ${before.channel.name}`);
             await before.channel.delete();
         }
     }
 
     if (after.channel != null && after.channel.name === magic_channel_name) {
-
-        new_channel = create_channel_for_member(after.member, after.channel);
+        
+        create_channel_for_member(after.member, after.channel);
 
     }
 
 });
 
 async function create_channel_for_member(member, template_channel) {
-    new_channel = await template_channel.clone({
-        name: `${channel_prefix} ${channel_names[Math.floor(channel_names.length * Math.random())]}`,
+    let new_name = `${channel_prefix} ${channel_names[Math.floor(channel_names.length * Math.random())]}`;
+    console.log(`Creating channel ${new_name} for ${member.user.tag}`);
+    let new_channel = await template_channel.clone({
+        name: new_name,
         reason: 'Auto Voice Channel',
         position: 999
     });
